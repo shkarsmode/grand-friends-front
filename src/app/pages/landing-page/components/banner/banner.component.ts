34685032c7b2f5f49fea6d02457e476084a5082a
Shkarupa Daniil <zshkarrr@outlook.com>
2024-03-24 17:03:55 +0200

    Add about info page

--- src/app/pages/landing-page/components/banner/banner.component.ts
+++ src/app/pages/landing-page/components/banner/banner.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Injector } from '@angular/core';
+import { CommonLandingComponentClass } from '../../../../shared';
 
 @Component({
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrl: './banner.component.scss'
+    selector: 'app-banner',
+    templateUrl: './banner.component.html',
+    styleUrl: './banner.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BannerComponent {
-
+export class BannerComponent extends CommonLandingComponentClass {
+    constructor(inject: Injector) {
+        super(inject);
+    }
 }
--- src/assets/data/landing-info.ts
+++ src/assets/data/landing-info.ts
@@ -11,7 +11,7 @@ export const landingInfo: ILandingInfo = {
 			tag: LandingBlocks.AdditionalBanner,
 			title: "Where <span class='marked'>Every Story</span> Matters",
 			description: "GrandFriends is a groundbreaking platform born from a simple, yet profound belief: every life is a tapestry of stories waiting to be told. In an age where digital connections often overshadow personal ones, we're on a mission to reverse the tide. <br /><br />GrandFriends bridges generations, turning moments into memories and wisdom into shared treasures.",
-			img: 'bg-banner-1.png'
+			bgImg: 'bg-banner-1.png'
 		},
 		{
 			tag: LandingBlocks.InfoBlock,
@@ -29,10 +29,17 @@ export const landingInfo: ILandingInfo = {
 		},
 		{
 			tag: LandingBlocks.InfoBlock,
+			title: "For the <span class='marked'>Community</span>",
+			description: "Our platform stands as a beacon for communities striving to weave tighter bonds of understanding and respect. <br /><br />By inviting seniors, cultural centers, and city organizations to join, we foster an environment where every elder's story is a lesson, and every young listener, a keeper of legacy. GrandFriends revitalizes the social fabric, one story at a time.",
+			img: 'info-group-3.png',
+			isImgRight: true
+		},
+		{
+			tag: LandingBlocks.InfoBlock,
 			title: "For <span class='marked'>Schools</span>",
 			description: "GrandFriends offers a program emphasizing empathy, listening, and engagement, going beyond traditional academics. It nurtures social skills alongside education, preparing students for positive societal contributions. <br /><br />Schools are invited to join, enhancing their educational offerings.",
-			img: 'info-group-3.png',
-			isImgRight: true,
+			img: 'info-group-4.png',
+			isImgRight: false,
 			isAdditionalPadding: true
 		},
 		{
@@ -41,6 +48,56 @@ export const landingInfo: ILandingInfo = {
 			description: "Whether you're an individual eager to share your story, a school looking to broaden your students' horizons, or a community center aiming to bridge generational gaps, GrandFriends welcomes you. <br /><br /> Together, let's turn every life into a legacy, ensuring that no story goes untold, and no voice, unheard. <br /><br /> <strong>Because Every Story Matters.</strong>",
 			img: 'phone.png',
 			bgImg: 'bg-ads-1.png',
+			isAdditionalPadding: true
+		},
+	],
+	'about': [
+		{
+			tag: LandingBlocks.Banner,
+			title: "<span class='marked'>About Us: <br /></span> The GrandFriends Story",
+			img: 'about-us-banner.png',
+		},
+		{
+			tag: LandingBlocks.AdditionalBanner,
+			title: "<span class='marked'>The Genesis</span> of GrandFriends",
+			description: "Our founding principle was simple yet profound: to ensure that no story, no matter how seemingly ordinary, goes untold. <br /><br />We recognized the power of narrative in bridging generations, cultures, and distances, fostering a world where every life is acknowledged, celebrated, and learned from.",
+			bgImg: 'additional-banner-1.png'
+		},
+		{
+			tag: LandingBlocks.InfoBlock,
+			title: "<span class='marked'>Our Mission</span>",
+			description: "At the heart of GrandFriends is the mission to weave the world's largest tapestry of memorable moments. We believe in the uniqueness and value of every individual's journey.<br /><br />Our platform serves as a global stage for personal biographies, allowing everyone — from the young student in a bustling city to the elder in a quiet village — to document, share, and preserve their life stories. It's more than a collection; it's a celebration of humanity in all its diversity.",
+			img: 'about-info-group-1.png',
+			isImgRight: true
+		},
+		{
+			tag: LandingBlocks.InfoBlock,
+			title: "<span class='marked'>Innovation </span> Through Artificial Intelligence ",
+			description: "Understanding that meaningful connections are fostered through thoughtful dialogue, GrandFriends harnesses advanced AI to enrich the storytelling experience. <br /> <br />Our technology thoughtfully analyzes previous conversations, using insights to nudge questions and prompts that delve deeper, encouraging a rich exchange of experiences and wisdom. This AI-driven approach ensures that each interaction is not just a conversation, but a journey of discovery and understanding.",
+			img: 'about-info-group-2.png',
+			isImgRight: false
+		},
+		{
+			tag: LandingBlocks.InfoBlock,
+			title: "For <span class='marked'>A Vision</span> for Every Story",
+			description: "Our vision extends beyond digital interaction; it's about creating a legacy. GrandFriends aspires for every human to have their own personal biography — living life while capturing the essence of their existence. <br /><br />From laughter - filled moments to challenges overcome, we champion the belief that every story matters and every story is special.",
+			img: 'about-info-group-3.png',
+			isImgRight: true
+		},
+		{
+			tag: LandingBlocks.InfoBlock,
+			title: "The World's <span class='marked'> Repository of Life</span> Stories",
+			description: "Imagine a repository where millions of stories from across the globe are preserved in their own words. That's the future GrandFriends is building. <br />A future where the tapestry of human experience is accessible to all, offering endless lessons, inspirations, and connections. It's a bold ambition: to host the world's largest collection of personal stories, each a testament to a life lived and the million stories it encompasses.",
+			img: 'about-info-group-4.png',
+			isImgRight: false,
+			isAdditionalPadding: true
+		},
+		{
+			tag: LandingBlocks.AdsBlock,
+			title: "Join Us in Celebrating Every Story",
+			description: "GrandFriends is more than a platform; it's a movement towards a more empathetic and connected world. A world where every person's story is valued, where generational divides are bridged through shared narratives, and where the richness of human experience is acknowledged and celebrated. <br /><br /> We invite you to be part of this journey. Share your story, discover the tales of others, and join us in creating a legacy of stories that illuminate the beauty and diversity of life itself. Because a life lived is indeed a million stories waiting to be told.",
+			img: 'phone.png',
+			bgImg: 'bg-ads-2.png',
 			isAdditionalPadding: true
 		},
 	]
